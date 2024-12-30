ff7930b21ad6947749b191c37a0202e00a26edd6
EVE <38647033+EmmiitBrown@users.noreply.github.com>
2024-12-30 00:08:12 -1000

    Initial commit

---
+++ mt80437.go
@@ -0,0 +1,5 @@
+package main // Compulsary statement
+import "fmt" // Include package "format"
+import "rsc.io/quote" // makes the beginning point of a golang function.
+func main() {fmt.Println("Welcome home Primary User 1")} // Prints on screen
+godoc func // main resource
