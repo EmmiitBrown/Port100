5cca57a36eaaf874ff2639e1b41b9180b1bb4713
White_White <38647033+EmmiitBrown@users.noreply.github.com>
2026-03-12 19:55:39 -0600

    Update mt80437.go
    Needs word wrap markdown also cs. Csharp

--- mt80437.go
+++ mt80437.go
@@ -3,3 +3,36 @@ import "fmt" // Include package "format"
 import "rsc.io/quote" // makes the beginning point of a golang function.
 func main() {fmt.Println("Welcome home Primary User 1")} // Prints on screen
 godoc func // main resource
+$diag22=
+$diag45=
+0x50 01
+0x45 01
+0x50 00
+0x50 01
+0x50 01
+0x45 01
+0x50 00
+0x50 01
+mov 'rtn1
+imm_ 666
+0x50 01
+0x45 01
+0x50 00
+0x50 01
+0x50 01
+0x45 01
+0x50 00
+0x50 01
+0x50 01
+0x45 01
+0x50 00
+0x50 01
+RTN1 cp0x55
+0x50 01
+0x45 01
+0x50 00
+0x50 01
+func{Standard_imm
+useQoS(5) -Y Dave_str: 55xxxx;
+diag if=$
+diag if=used
